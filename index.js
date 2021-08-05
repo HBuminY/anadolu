586b9ceeb8ba4e1092ff00ff41877daec4206b4c
HBuminY <70579311+HBuminY@users.noreply.github.com>
2021-08-05 04:31:28 +0300

    added accounts, developed ws and added reg page

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//#region settings
+const portMode = "test"  // test or build as string
+//#endregion
+
+
 //#region imports and stuff
 // EXPRESS
 const express = require("express");
@@ -11,27 +16,72 @@ app.use("/", router);
 const http = require("http");
 const server = http.createServer(app);
 
-const PORT = process.env.PORT;
+let PORT = -1;
+if(portMode == "test"){
+    PORT = 8585;
+}else if(portMode == "build"){
+    PORT = process.env.PORT;
+};
 
 // WEBSOCKETS
 const WebSocketServer = require("ws").Server;
-const wss = new WebSocketServer({server: server, path: "/ws"});
+const ws = new WebSocketServer({server: server, path: "/ws"});
 
 //FILESYSTEM
 const fs = require("fs");
 //#endregion
 
-//#region EXPRESS EVENTS
-app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/views/home.html");
+
+//#region FILE SYSTEM
+let accountsObj;
+
+fs.readFile(__dirname+"/database/accounts.json", (err, data)=>{
+    accountsObj = JSON.parse(data);
 });
 //#endregion
 
-//#region WS EVENTS
-wss.on('connection', (ws)=>{
-    console.log("someone connected");
+
+//#region EXPRESS EVENTS
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/public/home.html");
 });
 //#endregion
+
+
+//#region WS EVENTS
+ws.on('connection', (ws)=>{
+    console.log("someone connected");
+
+    ws.on('message', (message)=>{
+        try {
+            let msg = JSON.parse(message);
+            if(msg.type=="regObj"){
+                console.log("register data arrived");
+
+                let data = msg.data;
+                let username = data.username;
+                let password = data.password;
+
+                const registerCondition = [
+                    accountsObj[username]==undefined,
+                    username.length<=10,
+                    username.length>=3
+                ];
+
+                if (!registerCondition.includes(false)) {
+                    console.log("valid register");
+                }
+
+                ws.send("register request recieved");
+            };
+
+        } catch (e) {
+            console.log("unvalid message", e);
+        };
+    });
+});
+//#endregion
+
 
 //#region LISTEN FOR REQUESTS
 const listener = server.listen(PORT, ()=>{
---
+++ public/sources/register.js
@@ -0,0 +1,36 @@
+let url = `ws://${window.location.hostname}:${window.location.port}/ws`
+let ws = new WebSocket(url);
+
+/*
+This website does not collect not send your ip to any server.
+The script below is created as part of a feature that's to be added in the future for safety of the service.
+
+ws.onopen = () => {
+    console.log("connected!");
+    //
+    fetch("https://api.db-ip.com/v2/free/self")
+      .then(response => response.json())
+      .then(data => {
+        ws.send(JSON.stringify(data));
+      });
+};
+*/
+
+ws.onmessage = (msg)=>{
+    console.log(msg.data);
+};
+
+function register(){
+    let nameElem = document.getElementById("nameInp");
+    let pswElem = document.getElementById("pswInp");
+
+    let regObject = {
+        "type":"regObj",
+        "data":{
+            "username":nameElem.value,
+            "password":pswElem.value
+        }
+    }
+    nameElem.value=""; pswElem.value="";
+    ws.send(JSON.stringify(regObject));
+};
